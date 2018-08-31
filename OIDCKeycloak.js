41531ebc0863000d3b72fd191bf790c1de636169
David Festal <dfestal@redhat.com>
2018-08-31 11:26:11 +0200

    Update OIDCKeycloak.js

--- OIDCKeycloak.js
+++ OIDCKeycloak.js
@@ -210,7 +210,7 @@
         }
 
         kc.login = function (options) {
-            options.redirectUri = 'https://raw.githubusercontent.com/davidfestal/support-fixed-redirect-uris-in-che/master/oidcCallback.html';
+            options.redirectUri = 'https://rawgit.com/davidfestal/support-fixed-redirect-uris-in-che/master/oidcCallback.html';
             window.sessionStorage.setItem('oidcDashboardRedirectUrl', window.location.href);
             return adapter.login(options);
         }
